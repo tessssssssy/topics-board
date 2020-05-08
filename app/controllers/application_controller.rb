235d89554da721f566ce2e83e684e1940786ad24
tessssssssy <tessfabry@gmail.com>
2020-05-08 12:58:29 +1000

    fixed user sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,12 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     flash[:authorization_error] = "❌ Not authorized! ❌"
     redirect_to topics_path
-   end 
+   end
+   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end 
 end
